370f2972583622714bbe339315e9a73f27afe4c3
BLOBADOODLE <lucahtaylor@gmail.com>
2022-01-31 20:48:43 +0000

    Send welcome message on guild join

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,15 @@
 const log = require('../log.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = (client, guild) => {
+
+	const embed = new MessageEmbed()
+		.setColor('#FF0000')
+		.setTitle('Welcome!')
+		.setDescription('Hello! My default prefix is \`&\`. You can editing the settings with \`&set\` command.')
+
+	const syschannel = guild.systemChannel;
+
+	syschannel.send({embeds: [embed]});
 	log.info(`${guild.id} added the bot. Owner: ${guild.ownerId}`);
 }
