01919066973ad0f71ec80bab6ddaf8b125089019
BLOBADOODLE <lucahtaylor@gmail.com>
2022-06-20 16:49:16 +0100

    Organise button handling

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -50,120 +50,128 @@ async function handleSelect(interaction) {
 	}
 }
 
-async function handleButton(interaction) {
+function handleButton(interaction) {
 	if(interaction.customId === 'next' || interaction.customId === 'prev') {
-		const e6 = new yiff(process.env.E6_USER, process.env.E6_KEY, `e621bot/1.0 (by ${process.env.E6_USER})`);
-
-		const msg = interaction.message;
-		const embed = msg.embeds[0];
-		const footer = embed.footer.text;
-
-		let pageStr, tagStr;
-		[pageStr, tagStr] = footer.split('\n');
-		const cpage = parseInt(pageStr.substring(6));
-		let tags = tagStr.substring(8).split(' ');
-		let page = cpage;
-
-		if(interaction.customId === 'next') page++;
-		else page--;
-
-		const row = new MessageActionRow().addComponents(
-			new MessageButton()
-				.setCustomId('prev')
-				.setLabel('prev')
-				.setStyle('PRIMARY'),
-			new MessageButton()
-				.setCustomId('next')
-				.setLabel('next')
-				.setStyle('PRIMARY'),
-		);
-
-		const prevrow = new MessageActionRow().addComponents(
-			new MessageButton()
-				.setCustomId('prev')
-				.setLabel('prev')
-				.setStyle('PRIMARY'),
-		);
-
-		const settings = getSettings(msg.guild);
-
-		const blacklist = settings.serverBlacklist;
-	
-		let stags = [...tags];
-
-		for(i in blacklist) {
-			stags.push(`-${blacklist[i]}`)
-		}
-
-		const posts = await e6.search(stags, 1, page);
-		const post = posts.data[0];
-
-		const emptyembed = new MessageEmbed()
-			.setColor(e6.randcol())
-			.setTitle('e621.net')
-			.setDescription('Nobody here but us chickens!')
-			.setFooter({text: `Page: ${page}\nSearch: ${tags.join(' ')}`})
-
-		if(!posts.data.length) { 
-			interaction.deferUpdate();
-			return msg.edit({embeds: [emptyembed], components: [prevrow]});
-		}
-
-		if(!posts.ok) return msg.edit('A server error was encountered. Perhaps e621 is down?');
-
-		const NewEmbed = new MessageEmbed()
-			.setColor(e6.randcol())
-			.setTitle('e621.net')
-			.setURL(`https://e621.net/posts/${post.id}`)
-			.addFields(
-				{name: 'Score', value: String(post.score.total), inline: true},
-				{name: 'Favourites', value: String(post.fav_count), inline: true},
-				{name: 'Comments', value:String(post.comment_count), inline: true}
-			)
-			.setImage(post.file.url)
-			.setFooter({text: `Page: ${page}\nSearch: ${tags.join(' ')}`});
-
-		interaction.deferUpdate();
-		return msg.edit({'embeds': [NewEmbed], components: [row]})
+		handleNextPrev(interaction);
 	} else if (interaction.customId === 'delete') {
 		interaction.deferUpdate();
 		interaction.message.delete();
 	} else if (interaction.customId === 'show') {
-		const e6 = new yiff(process.env.E6_USER, process.env.E6_KEY, `e621bot/1.0 (by ${process.env.E6_USER})`);
-
-		const msg = interaction.message;
-		const embed = msg.embeds[0];
-		const footer = embed.footer.text;
-
-		const id = parseInt(footer.substring(4))
-
-		const post = await e6.getpost(id);
-
-		if(post.status === 404) return msg.edit({content: 'Nobody here but us chickens!'}) // TODO: make these appear in the embed not in the contetn
-
-		if (!post.ok) return msg.edit({content: 'A server error was encountered. Perhaps e621 is down?'});
-
-		const newEmbed = new MessageEmbed()
-			.setColor(e6.randcol())
-			.setTitle('e621.net')
-			.setURL(`https://e621.net/posts/${post.data.id}`)
-			.addFields(
-				{name: 'Score', value: String(post.data.score.total)},
-				{name: 'Favourites', value: String(post.data.fav_count)},
-				{name: 'Comments', value: String(post.data.comment_count)}
-			)
-			.setImage(post.data.file.url ?? '')
-			.setFooter({text: `ID: ${post.data.id}`})
-			.setAuthor({name: post.data.tags.artist.join(' ')})
-
-		const row = new MessageActionRow().addComponents(
-			new MessageButton()
-				.setCustomId('delete')
-				.setLabel('Hide')
-				.setStyle('SECONDARY'),
-		);
-
-		interaction.deferUpdate();
-		return msg.edit({embeds: [newEmbed], components: [row]});
+		handleShow(interaction);
 	}
+}
+
+async function handleNextPrev(interaction) {
+	const e6 = new yiff(process.env.E6_USER, process.env.E6_KEY, `e621bot/1.0 (by ${process.env.E6_USER})`);
+
+	const msg = interaction.message;
+	const embed = msg.embeds[0];
+	const footer = embed.footer.text;
+
+	let pageStr, tagStr;
+	[pageStr, tagStr] = footer.split('\n');
+	const cpage = parseInt(pageStr.substring(6));
+	let tags = tagStr.substring(8).split(' ');
+	let page = cpage;
+
+	if(interaction.customId === 'next') page++;
+	else page--;
+
+	const row = new MessageActionRow().addComponents(
+		new MessageButton()
+			.setCustomId('prev')
+			.setLabel('prev')
+			.setStyle('PRIMARY'),
+		new MessageButton()
+			.setCustomId('next')
+			.setLabel('next')
+			.setStyle('PRIMARY'),
+	);
+
+	const prevrow = new MessageActionRow().addComponents(
+		new MessageButton()
+			.setCustomId('prev')
+			.setLabel('prev')
+			.setStyle('PRIMARY'),
+	);
+
+	const settings = getSettings(msg.guild);
+
+	const blacklist = settings.serverBlacklist;
+
+	let stags = [...tags];
+
+	for(i in blacklist) {
+		stags.push(`-${blacklist[i]}`)
+	}
+
+	const posts = await e6.search(stags, 1, page);
+	const post = posts.data[0];
+
+	const emptyembed = new MessageEmbed()
+		.setColor(e6.randcol())
+		.setTitle('e621.net')
+		.setDescription('Nobody here but us chickens!')
+		.setFooter({text: `Page: ${page}\nSearch: ${tags.join(' ')}`})
+
+	if(!posts.data.length) { 
+		interaction.deferUpdate();
+		return msg.edit({embeds: [emptyembed], components: [prevrow]});
+	}
+
+	if(!posts.ok) return msg.edit('A server error was encountered. Perhaps e621 is down?');
+
+	const NewEmbed = new MessageEmbed()
+		.setColor(e6.randcol())
+		.setTitle('e621.net')
+		.setURL(`https://e621.net/posts/${post.id}`)
+		.addFields(
+			{name: 'Score', value: String(post.score.total), inline: true},
+			{name: 'Favourites', value: String(post.fav_count), inline: true},
+			{name: 'Comments', value:String(post.comment_count), inline: true}
+		)
+		.setImage(post.file.url)
+		.setFooter({text: `Page: ${page}\nSearch: ${tags.join(' ')}`});
+
+	interaction.deferUpdate();
+	return msg.edit({'embeds': [NewEmbed], components: [row]})
+}
+
+async function handleShow(interaction) {
+	const e6 = new yiff(process.env.E6_USER, process.env.E6_KEY, `e621bot/1.0 (by ${process.env.E6_USER})`);
+
+	const msg = interaction.message;
+	const embed = msg.embeds[0];
+	const footer = embed.footer.text;
+
+	const id = parseInt(footer.substring(4))
+
+	const post = await e6.getpost(id);
+
+	if(post.status === 404) return msg.edit({content: 'Nobody here but us chickens!'}) // TODO: make these appear in the embed not in the contetn
+
+	if (!post.ok) return msg.edit({content: 'A server error was encountered. Perhaps e621 is down?'});
+
+	const newEmbed = new MessageEmbed()
+		.setColor(e6.randcol())
+		.setTitle('e621.net')
+		.setURL(`https://e621.net/posts/${post.data.id}`)
+		.addFields(
+			{name: 'Score', value: String(post.data.score.total)},
+			{name: 'Favourites', value: String(post.data.fav_count)},
+			{name: 'Comments', value: String(post.data.comment_count)}
+		)
+		.setImage(post.data.file.url ?? '')
+		.setFooter({text: `ID: ${post.data.id}`})
+		.setAuthor({name: post.data.tags.artist.join(' ')})
+
+	const row = new MessageActionRow().addComponents(
+		new MessageButton()
+			.setCustomId('delete')
+			.setLabel('Hide')
+			.setStyle('SECONDARY'),
+	);
+
+	interaction.deferUpdate();
+	return msg.edit({embeds: [newEmbed], components: [row]});
 }
